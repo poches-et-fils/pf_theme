c3c96ddda301ea92ee398ddebb04298ce8241aac
Matt Dunbar <matt@mattdsworld.com>
2018-06-04 19:12:07 +0700

    dist shouldn't be committed, it's basically the compiled version of the theme

--- dist/assets/shuffle-dom-siblings.js
+++
@@ -1,7 +0,0 @@
-const shuffleDOMSiblings = containerName => {
-  const parent = $(containerName)
-  const divs = parent.children()
-  while (divs.length) {
-    parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0])
-  }
-}

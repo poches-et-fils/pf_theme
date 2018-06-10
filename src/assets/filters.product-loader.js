6130bac9cff34175f890e24dc2cdeb0e07f22a4d
Matt Dunbar <matt@mattdsworld.com>
2018-06-10 18:31:21 +0700

    improved product loader, use JSON

--- src/assets/filters.product-loader.js
+++ src/assets/filters.product-loader.js
@@ -21,53 +21,48 @@ window.poches.filters['productLoader'] = function () {
     if(filterData === false) {
       throw new Error('Filter Data Must Be Set Before Loading Data.');
     }
-    $.getJSON('/collections/all-designs/products.json?limit=250&page=' + pageNum, function (response) {
-      if(response['products'].length > 0) {
-        for(var i = 0;i < response['products'].length; i++) {
-          var productAttributes = {};
-          var productImage = '';
-          if(response['products'][i]['images'] && response['products'][i]['images'][0]) {
-            productImage = response['products'][i]['images'][0]['src'];
-          }
-          filterData.addDesignProduct(response['products'][i]['title'], productImage, response['products'][i]['handle']);
+    var response = {'products': window.collectionData['all-designs']};
+    if(response['products'].length > 0) {
+      for(var i = 0;i < response['products'].length; i++) {
+        var productAttributes = {};
+        var productImage = '';
+        if(response['products'][i]['images'] && response['products'][i]['images'][0]) {
+          productImage = response['products'][i]['images'][0];
         }
-        window.poches.filters.productLoader.loadProductPage(pageNum+1, callback);
-      } else {
-        window.poches.filters.productsLoaded = true;
-        if(callback !== false) callback();
+        filterData.addDesignProduct(response['products'][i]['title'], productImage, response['products'][i]['handle']);
       }
-    })
+
+      window.poches.filters.productsLoaded = true;
+      if(callback !== false) callback();
+    }
   }
 
   this.loadStandardProductsPage = function (collection, tags, pageNum = 1, callback = false) {
     if(filterData === false) {
       throw new Error('Filter Data Must Be Set Before Loading Data.');
     }
-    $.getJSON('/collections/' + collection + '/products.json?limit=250&page=' + pageNum, function (response) {
-      if(response['products'].length > 0) {
-        for(var i = 0;i < response['products'].length; i++) {
-          var productMatch = true;
-          if(tags.length == 1 && tags[0] == '') {
-            tags = [];
-          }
-          for(var j = 0;j < tags.length;j++) {
-            if(response['products'][i]['tags'].indexOf(tags[j]) === -1) {
-              productMatch = false;
-            }
-          }
-          if(productMatch) {
-            var productImage = '';
-            if(response['products'][i]['images'] && response['products'][i]['images'][0]) {
-              productImage = response['products'][i]['images'][0]['src'];
-            }
-            filterData.addGenericProduct(response['products'][i]['title'], productImage, response['products'][i]['handle'], response['products'][i]['variants'][0]['price'], response['products'][i]['variants'][0]['id']);
+    var response = {'products': window.collectionData[collection]};
+    if(response['products'].length > 0) {
+      for(var i = 0;i < response['products'].length; i++) {
+        var productMatch = true;
+        if(tags.length == 1 && tags[0] == '') {
+          tags = [];
+        }
+        for(var j = 0;j < tags.length;j++) {
+          if(response['products'][i]['tags'].indexOf(tags[j]) === -1) {
+            productMatch = false;
           }
         }
-        window.poches.filters.productLoader.loadStandardProductsPage(collection, tags, pageNum+1, callback);
-      } else {
-        window.poches.filters.productsLoaded = true;
-        if(callback !== false) callback();
+        if(productMatch) {
+          var productImage = '';
+          if(response['products'][i]['images'] && response['products'][i]['images'][0]) {
+            productImage = response['products'][i]['images'][0];
+          }
+          filterData.addGenericProduct(response['products'][i]['title'], productImage, response['products'][i]['handle'], response['products'][i]['variants'][0]['price'], response['products'][i]['variants'][0]['id']);
+        }
       }
-    })
+      window.poches.filters.productsLoaded = true;
+      if(callback !== false) callback();
+    }
   }
 }

3b108a2fcd003e975c7313d11cee75563e77e02b
Andrew Gulik <andrewgulik@gmail.com>
2018-06-27 11:41:18 -0400

    fetch repo, set up env, as well as animate toggles on product page

--- src/assets/toggle-handler.js
+++ src/assets/toggle-handler.js
@@ -1,16 +1,23 @@
 const toggleProductInfo = event => {
-  const container = $(event.target).closest('.toggle--item')
-  const currentArrow = container.find('img.toggle--icon--plus') || false
-  const plusArrow = 'https://cdn.shopify.com/s/files/1/0013/6685/1647/files/plus.svg'
-  const minusArrow = 'https://cdn.shopify.com/s/files/1/0013/6685/1647/files/minus.svg'
+  const container = $(event.target).closest(".toggle--item");
+  const currentArrow = container.find("img.toggle--icon--plus") || false;
+  const plusArrow =
+    "https://cdn.shopify.com/s/files/1/0013/6685/1647/files/plus.svg";
+  const minusArrow =
+    "https://cdn.shopify.com/s/files/1/0013/6685/1647/files/minus.svg";
 
-  container
-  .find('.toggle--contents')
-  .toggle()
-  
+  container.find(".toggle--contents").toggle("fast");
+
   if (currentArrow) {
-    currentArrow.attr('src', currentArrow.attr('src').includes(plusArrow) ? minusArrow : plusArrow)
+    currentArrow.attr(
+      "src",
+      currentArrow.attr("src").includes(plusArrow) ? minusArrow : plusArrow
+    );
   }
-}
+};
 
-$(document).on('click', '.toggle--heading, *[class^=toggle--icon]', toggleProductInfo)
+$(document).on(
+  "click",
+  ".toggle--heading, *[class^=toggle--icon]",
+  toggleProductInfo
+);

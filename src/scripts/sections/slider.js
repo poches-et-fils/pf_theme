60255205be4b967b20c62aabbf10530efcb6a0a6
Dan Webb <mail@danwebb.co>
2018-08-30 18:44:01 +0700

    Stop glide running if no slides have been added to the homepage slider

--- src/scripts/sections/slider.js
+++ src/scripts/sections/slider.js
@@ -1,5 +1,10 @@
 theme.Slider = function(container) {
 	const $container = $(container);
+
+	if ($(slider).find('.glide__slide').length === 0) {
+		return;
+	}
+
 	const speed = $container.data('slider-speed');
 	const slider = `[data-section-type="${$container.data('section-type')}"].glide`;
 	const sliderOptions = {

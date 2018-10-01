6aab4cc77aba5b7b1319c5c860f306a2e801bea5
Dan Webb <mail@danwebb.co>
2018-10-01 12:06:36 +0100

    Update readme with new docs

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -16762,7 +16762,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54606' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '50086' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 

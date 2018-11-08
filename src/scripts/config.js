970f8e3442ce17691e5b36394085727526679635
Dan Webb <mail@danwebb.co>
2018-11-08 13:41:50 +0000

    Add production algolia config

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -10,11 +10,11 @@ const config = {
 	production: {
 		algolia: {
 			appId: '7M9U4OP0D8',
-			apiKey: 'dc5c134cd92b8d6fdaff3232cb7c9e83',
-			index: 'poches_dev_products'
+			apiKey: '0fa9a8c172edb3c3f5ddedffbd0f8fbb',
+			index: 'poches_fr_products'
 		}
 	}
 };
 
 // Check snippets/global-theme-js for the env variable
-export default config[window.theme.env];
+export default config[window.theme.env || 'production'];

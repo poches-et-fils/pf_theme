4fcf8dc771a6662c2f3c53469ae3e75cbefc8c1e
poches-et-fils <richardbonneau@hotmail.com>
2018-12-21 18:51:14 -0500

    changed css on button to fit new hero

--- src/assets/scripts.js
+++ src/assets/scripts.js
@@ -19538,7 +19538,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54913" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55667" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
